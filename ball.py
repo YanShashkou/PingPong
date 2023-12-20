5e2c00ce446dcde8aaf8e9f1384ff9670ae389f7
yan1122 <yan.tiger8@gmail.com>
2023-12-20 10:14:14 +0100

    rr

---
+++ ball.py
@@ -0,0 +1,18 @@
+import turtle
+
+class Ball(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.color('white')
+        self.x_move = 0.1
+        self.y_move = 0.1
+    def move(self):
+        x_cord = self.xcor() + self.x_move
+        y_cord = self.ycor() + self.y_move
+        self.goto(x_cord, y_cord)
+
+
+    def bounce(self):
+        self.y_move *= -1
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 import paddle
+import ball
 
 screen = turtle.Screen()
 screen.setup(800,600)
@@ -9,17 +10,16 @@ screen.title('Pong')
 
 r_paddle= paddle.Paddle((350,0))
 l_paddle= paddle.Paddle((-350,0))
+ball = ball.Ball()
 is_game_cont = True
 while is_game_cont:
     screen.listen()
     screen.onkeypress(key='w',fun=l_paddle.up)
     screen.onkeypress(key='s',fun=l_paddle.down)
+    ball.move()
     screen.update()
-
-
-
-
-
+    if ball.ycor() > 280:
+        ball.bounce()
 
 
 
